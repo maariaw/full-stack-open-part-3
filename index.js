5dc5e42ad1c89ab201a4ec107187e9c6d2f97a74
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-05-18 13:36:30 +0300

    Finish up to step 8 of phonebook backend

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const { request } = require('express')
+const { request, response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan(
+    ':method :url :status :res[content-length] - :response-time ms :body'
+))
+
+morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
 
 let persons = [
     {
