6648d444b820d783421df14bf09b9af871d260e5
Maaria Wahlström <65168298+maariaw@users.noreply.github.com>
2021-05-18 15:42:20 +0300

    Make app Heroku-ready

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { request, response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan(
     ':method :url :status :res[content-length] - :response-time ms :body'
 ))
@@ -90,7 +92,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
